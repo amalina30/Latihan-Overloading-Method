e8ebb02f232f891b4207b0fadfd4653196b11d70
amalina30 <amalina3037@gmail.com>
2022-04-07 11:04:24 +0700

    selesai

---
+++ LatihanOverloadingMethod/Numbers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LatihanOverloadingMethod
+{
+    public class Numbers
+    {
+        private int _x, _y;
+        public int FindMinimum(int number1, int number2)
+        {
+            return number1 < number2 ? number1 : number2;
+        }
+
+        public int FindMinimum(int number1, int number2, float number3)
+        {
+            _x = number1 < number3 ? number1 : (int)number3;
+            _y = number2 < number3 ? number2 : (int)number3;
+            return number1 < number2 ? _x : _y;
+        }
+
+        public int FindMaximum(int number1, int number2)
+        {
+            return number1 > number2 ? number1 : number2;
+        }
+
+        public int FindMaximum(int number1, int number2, float number3)
+        {
+            _x = number1 > number3 ? number1 : (int)number3;
+            _y = number2 > number3 ? number2 : (int)number3;
+            return number1 > number2 ? _x : _y;
+        }
+    }
+}
